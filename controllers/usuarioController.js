d8d028b090308c20fc88835546e62c4988a604ad
ArturoLuna7 <arturo712luna@gmail.com>
2023-10-06 15:46:45 -0700

    feat-encryptAlgorithm
    método de encriptación

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,22 +1,26 @@
 const Sequelize = require('sequelize').Sequelize;
+const EncriptadorService = require('../Services/encriptadorService');
 
-const User = require('../models/usuario')
+const User = require('../models/usuario');
  
-///Metodo para agregar un usuario a la base de datos 
-exports.addUserPromise = (id,nombre_usuario,correo,contrasena) => {
-     User.create({
-        id: id,
-        nombre_usuario: nombre_usuario,
-        correo: correo,
-        contrasena: contrasena 
-     })
-     .then(result => {
-        console.log(result);
-     })
-     .catch(err => {
-        console.log(err);
-     })
-}
+///Metodo para agregar un usuario a la base de datos con encriptación de contraseña
+exports.addUserPromise = async (id, nombre_usuario, correo, contrasena) => {
+   try {
+     const contraseñaEncriptada = await EncriptadorService.encriptarContraseña(contrasena);
+
+     const nuevoUsuario = await User.create({
+       id: id,
+       nombre_usuario: nombre_usuario,
+       correo: correo,
+       contrasena: contraseñaEncriptada, 
+     });
+     
+     console.log(nuevoUsuario);
+   } catch (error) {
+     console.log(error);
+   }
+ };
+ 
 
 // Metodo para obtener la lista de los usuarios en la base de datos
 exports.getUsers = () => {
