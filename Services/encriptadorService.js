be62b1f0567beaf51001b3e779872f851cd17859
ArturoLuna7 <arturo712luna@gmail.com>
2023-10-18 12:35:18 -0700

    fix: encriptadorService

--- Services/encriptadorService.js
+++ Services/encriptadorService.js
@@ -1,19 +1,19 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // Importa la biblioteca 'bcrypt'.
 
 class EncriptadorService {
   static encriptarContraseña(contraseña) {
-    const rondas = 10;
+    const rondas = 10; // Define el número de rondas para el hash (fijo en 10).
 
-    return new Promise((resolve, reject) => {
-      bcrypt.hash(contraseña, rondas, (err, contraseñaEncriptada) => {
+    return new Promise((resolve, reject) => { // Crea y devuelve una promesa.
+      bcrypt.hash(contraseña, rondas, (err, contraseñaEncriptada) => { // Utiliza bcrypt para encriptar la contraseña.
         if (err) {
-          reject(err); // En caso de error, rechaza la promesa
+          reject(err); // Rechaza la promesa si ocurre un error durante la encriptación.
         } else {
-          resolve(contraseñaEncriptada); // Si tiene éxito, resuelve la promesa con la contraseña encriptada
+          resolve(contraseñaEncriptada); // Resuelve la promesa con la contraseña encriptada si tiene éxito.
         }
       });
     });
   }
 }
 
-module.exports = EncriptadorService;
+module.exports = EncriptadorService; // Exporta la clase EncriptadorService para su uso en otros módulos.
