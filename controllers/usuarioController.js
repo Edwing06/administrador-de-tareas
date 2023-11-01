05aa62fa2e76a2a208d7dc7b147236413fdae468
ArturoLuna7 <arturo712luna@gmail.com>
2023-10-31 21:22:49 -0700

    feat: errors

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,89 +1,103 @@
-const Usuario = require('../models/usuario'); // Importa el modelo de usuario
-const bcrypt = require('bcrypt');
+const Usuario = require('../models/usuario')
+const bcrypt = require('bcrypt')
+const CustomError = require('../services/customError') // Importa la clase CustomError
+const errorHandler = require('../services/error') // Importa el middleware errorHandler
 
 // Controlador para obtener todos los usuarios
-exports.listar = async (req, res) => {
+exports.listar = async (req, res, next) => {
   try {
-    const usuarios = await Usuario.findAll(); // Obtiene todos los usuarios de la base de datos
-    res.json(usuarios); // Responde con los usuarios en formato JSON
+    const usuarios = await Usuario.findAll()
+    res.json(usuarios)
   } catch (error) {
-    console.error('Error al obtener usuarios:', error);
-    res.status(500).json({ error: 'Error interno del servidor' }); // Maneja y responde con un error 500 en caso de problemas
+    console.error('Error al obtener usuarios:', error)
+    const customError = new CustomError('Error al obtener usuarios', 500)
+    next(customError) // Manejo del error con el middleware errorHandler
   }
-};
+}
 
 // Controlador para crear un nuevo usuario
-exports.crear = async (req, res) => {
-  const { nombre_usuario, correo, contrasena } = req.body;
+exports.crear = async (req, res, next) => {
+  const { nombre_usuario, correo, contrasena } = req.body
 
   try {
-    const nuevoUsuario = await Usuario.create({ nombre_usuario, correo, contrasena }); // Crea un nuevo usuario en la base de datos
-    res.status(201).json(nuevoUsuario); // Responde con el nuevo usuario creado y un estado 201 (Creado)
+    const nuevoUsuario = await Usuario.create({
+      nombre_usuario,
+      correo,
+      contrasena
+    })
+    res.status(201).json(nuevoUsuario)
   } catch (error) {
-    console.error('Error al crear usuario:', error);
-    res.status(500).json({ error: 'Error interno del servidor' }); // Maneja y responde con un error 500 en caso de problemas
+    console.error('Error al crear usuario:', error)
+    const customError = new CustomError('Error al crear usuario', 500)
+    next(customError) // Manejo del error con el middleware errorHandler
   }
-};
+}
 
 // Controlador para obtener un usuario por su ID
-exports.obtenerPorId = async (req, res) => {
-  const { id } = req.params;
+exports.obtenerPorId = async (req, res, next) => {
+  const { id } = req.params
 
   try {
-    const usuario = await Usuario.findByPk(id); // Busca un usuario por su ID
+    const usuario = await Usuario.findByPk(id)
     if (!usuario) {
-      return res.status(404).json({ error: 'Usuario no encontrado' }); // Responde con un error 404 si el usuario no se encuentra
+      const customError = new CustomError('Usuario no encontrado', 404)
+      next(customError) // Manejo del error con el middleware errorHandler
+      return
     }
-    res.json(usuario); // Responde con los detalles del usuario en formato JSON
+    res.json(usuario)
   } catch (error) {
-    console.error('Error al obtener usuario por ID:', error);
-    res.status(500).json({ error: 'Error interno del servidor' }); // Maneja y responde con un error 500 en caso de problemas
+    console.error('Error al obtener usuario por ID:', error)
+    const customError = new CustomError('Error al obtener usuario por ID', 500)
+    next(customError) // Manejo del error con el middleware errorHandler
   }
-};
+}
 
 // Controlador para actualizar un usuario por su ID
-exports.actualizarPorId = async (req, res) => {
-  const { id } = req.params;
-  const { nombre_usuario, correo, contrasena } = req.body;
+exports.actualizarPorId = async (req, res, next) => {
+  const { id } = req.params
+  const { nombre_usuario, correo, contrasena } = req.body
 
   try {
-    const usuario = await Usuario.findByPk(id); // Busca un usuario por su ID
+    const usuario = await Usuario.findByPk(id)
     if (!usuario) {
-      return res.status(404).json({ error: 'Usuario no encontrado' }); // Responde con un error 404 si el usuario no se encuentra
+      const customError = new CustomError('Usuario no encontrado', 404)
+      next(customError) // Manejo del error con el middleware errorHandler
+      return
     }
 
-    usuario.nombre_usuario = nombre_usuario;
-    usuario.correo = correo;
+    usuario.nombre_usuario = nombre_usuario
+    usuario.correo = correo
 
-    // Encriptación de la contraseña actualizada
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(contrasena, saltRounds);
-    usuario.contrasena = hashedPassword;
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(contrasena, saltRounds)
+    usuario.contrasena = hashedPassword
 
-    await usuario.save(); // Guarda los cambios en el usuario
-
-    res.json(usuario); // Responde con los detalles del usuario actualizado en formato JSON
+    await usuario.save()
+    res.json(usuario)
   } catch (error) {
-    console.error('Error al actualizar usuario:', error);
-    res.status(500).json({ error: 'Error interno del servidor' }); // Maneja y responde con un error 500 en caso de problemas
+    console.error('Error al actualizar usuario:', error)
+    const customError = new CustomError('Error al actualizar usuario', 500)
+    next(customError) // Manejo del error con el middleware errorHandler
   }
-};
+}
 
 // Controlador para eliminar un usuario por su ID
-exports.eliminarPorId = async (req, res) => {
-  const { id } = req.params;
+exports.eliminarPorId = async (req, res, next) => {
+  const { id } = req.params
 
   try {
-    const usuario = await Usuario.findByPk(id); // Busca un usuario por su ID
+    const usuario = await Usuario.findByPk(id)
     if (!usuario) {
-      return res.status(404).json({ error: 'Usuario no encontrado' }); // Responde con un error 404 si el usuario no se encuentra
+      const customError = new CustomError('Usuario no encontrado', 404)
+      next(customError) // Manejo del error con el middleware errorHandler
+      return
     }
 
-    await usuario.destroy(); // Elimina el usuario de la base de datos
-
-    res.json({ mensaje: 'Usuario eliminado con éxito' }); // Responde con un mensaje de éxito en formato JSON
+    await usuario.destroy()
+    res.json({ mensaje: 'Usuario eliminado con éxito' })
   } catch (error) {
-    console.error('Error al eliminar usuario:', error);
-    res.status(500).json({ error: 'Error interno del servidor' }); // Maneja y responde con un error 500 en caso de problemas
+    console.error('Error al eliminar usuario:', error)
+    const customError = new CustomError('Error al eliminar usuario', 500)
+    next(customError) // Manejo del error con el middleware errorHandler
   }
-};
+}
