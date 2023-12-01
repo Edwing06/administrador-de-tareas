96bc2c5944c96bf5f715446e27be7dc5ce9eb575
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2023-12-01 01:36:58 -0700

    Update script.js
    Manejo de errores al poner credenciales inválidas

--- static/script.js
+++ static/script.js
@@ -17,3 +17,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+  document.getElementById('btnLogin').addEventListener('click', iniciarSesion);
+
+  function iniciarSesion(){
+    const correo = document.getElementById('correoLogin').value;
+    const contrasena = document.getElementById('contrasenaLogin').value;
+
+    // Realizar la solicitud de inicio de sesión al servidor
+    fetch('/autenticar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ correo, contrasena }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('No se pudo iniciar sesión. Credenciales inválidas.');
+        }
+        // Redirigir al usuario a la página deseada después de iniciar sesión correctamente
+        window.location.href = '/index'; // Reemplaza '/dashboard' con la ruta deseada
+    })
+    .catch(error => {
+        console.error('Error al iniciar sesión:', error.message);
+        alert(error.message); // Mostrar un mensaje de error al usuario
+    });
+  }
